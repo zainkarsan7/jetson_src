620db95fdfe16c6eb2b0f4bc22233199a72d25cd
zainkarsan7 <zainkarsan7@gmail.com>
2025-08-17 13:54:48 +0200

    test push

--- zain_pkg/src/hello_moveit.cpp
+++ zain_pkg/src/hello_moveit.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]){
 
 
     rclcpp::init(argc,argv);
-    auto const node = std::make_shared<rclcpp::Node>("hello_moveit",
+    auto const node = std::make_shared<rclcpp::Node>("hello_moveit basfs",
     rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true));
     auto const logger = rclcpp::get_logger("hello_moveit");
 
