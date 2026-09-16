70e3b1be95c6606dc4f108e2de75700e1fe4c1a2
zainkarsan7 <zainkarsan7@gmail.com>
2026-09-16 21:50:35 +0200

    getting back to moveit

---
+++ hb_robot_skills/src/inspect_scene_server.cpp
@@ -0,0 +1,11 @@
+#include <memory>
+#include <thread>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit_msgs/msg/move_it_error_codes.hpp>
+#include <hb_robot_interfaces/action/inspect_scene.hpp>
+
+int main(int argc, char **argv){
+    return 0;
+}
