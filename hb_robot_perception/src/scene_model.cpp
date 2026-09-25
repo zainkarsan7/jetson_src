d219e5fa566a461dd5c16f3bb9f9f5f602bb4ca3
zainkarsan7 <zainkarsan7@gmail.com>
2026-09-25 20:30:52 +0200

    more pcl

--- hb_robot_perception/src/scene_model.cpp
+++ hb_robot_perception/src/scene_model.cpp
@@ -19,12 +19,38 @@ namespace hb_perception{
         if(!ob.rgb || !ob.depth_to_rgb || !ob.rgb_camera_info){
             return false;
         }
+        auto camera_cloud = observationToCloud(ob);
+        if(!camera_cloud || camera_cloud->empty()){
+            return false;
+        }
 
-        
+
+
         
         return true;
     }
+    SceneModel::PointCloud::Ptr SceneModel::observationToCloud(const Observation& ob)const{
+        const auto& rgb = *ob.rgb;
+        const auto& depth = *ob.depth_to_rgb;
+        const auto& info = *ob.rgb_camera_info;
+    
 
+    // theres some encoding check here
+         if (depth.encoding != sensor_msgs::image_encodings::TYPE_16UC1){
+            return nullptr;
+         }
+
+         const double fx = info.k[0];
+         const double fy = info.k[4];
+         const double cx = info.k[2];
+         const double cy = info.k[5];
+
+         auto cloud = std::make_shared<PointCloud>();
+         cloud->reserve()
+
+            // PointCloud::Ptr transformObsCloud(const PointCloud &cloud, const Observation & ob)const;
+
+    }
     void SceneModel::clear(){
         observation_buffer_.clear();
         std::lock_guard<std::mutex>lock(scene_mutex);
